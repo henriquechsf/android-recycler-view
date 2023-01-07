606214fe33101ad765f7105aa65c3c446c0f4019
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2023-01-07 10:23:39 -0300

    connect adapter

--- app/src/main/java/tech/henriquedev/recyclerview/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/recyclerview/MainActivity.kt
@@ -2,10 +2,41 @@ package tech.henriquedev.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    var countryNameList = ArrayList<String>()
+    var detailsList = ArrayList<String>()
+    var imageList = ArrayList<Int>()
+
+    lateinit var adapter: CountriesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        countryNameList.add("United States")
+        countryNameList.add("Brazil")
+        countryNameList.add("Argentina")
+
+        detailsList.add("This is the United States Flag")
+        detailsList.add("This is the Brazil Flag")
+        detailsList.add("This is the Argentina Flag")
+
+        imageList.add(R.drawable.flag_usa)
+        imageList.add(R.drawable.flag_brazil)
+        imageList.add(R.drawable.flag_argentina)
+
+        adapter = CountriesAdapter(countryNameList, detailsList, imageList, this@MainActivity)
+
+        recyclerView.adapter = adapter
     }
 }
